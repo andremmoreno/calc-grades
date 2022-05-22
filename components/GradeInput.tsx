f13e54607a7d2d324ed561f0cf01604c0c96f394
André Moreno <andre.moreno@sdconecta.saudedigital.tech>
2022-05-22 15:42:50 -0300

    Página Baiana

---
+++ components/GradeInput.tsx
@@ -0,0 +1,43 @@
+import { NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, FormControl, FormLabel, FormHelperText, FormErrorMessage } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+type StringOrNumber = {valueAsString: string, valueAsNumber: number};
+
+type Props = {
+  textContent: string;
+  handleChange: (valueAsString: string, valueAsNumber: number) => void
+}
+
+const GradeInput: React.FC<Props> = ({
+  textContent,
+  handleChange
+}) => {
+
+  return (
+    <FormControl variant="floating" id="first-name">
+      <FormLabel>{ textContent }</FormLabel>
+      <NumberInput 
+        onChange={handleChange}
+        focusInputOnChange
+        defaultValue={6}
+        precision={2}
+        step={0.1}
+        max={10}
+        min={0}
+        allowMouseWheel
+        getAriaValueText={((value) => value.toLocaleString('pt-BR'))}
+      >
+        <NumberInputField/>
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+    </FormControl>
+
+  )
+}
+
+export default GradeInput
